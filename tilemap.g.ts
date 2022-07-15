27f183d4cab85d95b63b8c49832536ef4a43248e
Michael Thompson <actionscripted@gmail.com>
2022-07-15 12:44:53 -0400

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -10,6 +10,8 @@ namespace myTiles {
     export const tile4 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -191,6 +193,8 @@ namespace myTiles {
             case "tile4":return tile4;
             case "binaryRight":
             case "tile3":return tile3;
+            case "binaryMid":
+            case "tile5":return tile5;
         }
         return null;
     })
