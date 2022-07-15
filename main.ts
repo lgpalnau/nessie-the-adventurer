7cfff62ebed481dd8129afd0e719f783778792ed
smezzacapo <32824995+smezzacapo@users.noreply.github.com>
2022-07-15 12:46:12 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
     mySprite.vy = -300
 })
-let mySprite: Sprite = null
+let mySprite : Sprite = null
 scene.setBackgroundColor(9)
-tiles.setCurrentTilemap(tilemap`level1`)
+tiles.setCurrentTilemap(tilemap`
+    level1
+`)
 music.setVolume(29)
 music.playMelody("A F A B C5 G A G ", 120)
-mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`
+    myImage
+`, SpriteKind.Player)
 mySprite.setStayInScreen(true)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 500
 scene.cameraFollowSprite(mySprite)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 1))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 155))
