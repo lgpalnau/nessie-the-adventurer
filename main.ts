f736a27cccf52855cd1a143c8b4bf18d8c5ae014
Michael Thompson <actionscripted@gmail.com>
2022-07-15 12:59:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 let mySprite: Sprite = null
 scene.setBackgroundColor(9)
 tiles.setCurrentTilemap(tilemap`level1`)
+music.setVolume(29)
+music.playMelody("A F A B C5 G A G ", 120)
 mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 mySprite.setStayInScreen(true)
 controller.moveSprite(mySprite, 100, 0)
