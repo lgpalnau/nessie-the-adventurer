913273613dd466280341c8a4a48c44101892f78a
smezzacapo <32824995+smezzacapo@users.noreply.github.com>
2022-07-15 12:51:46 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def on_a_pressed():
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
 mySprite: Sprite = None
+myEnemySprite: Sprite = None
 scene.set_background_color(9)
 tiles.set_current_tilemap(tilemap("""
     level1
@@ -12,8 +13,13 @@ music.play_melody("A F A B C5 G A G ", 120)
 mySprite = sprites.create(assets.image("""
     myImage
 """), SpriteKind.player)
+myEnemySprite = sprites.create(assets.image("""
+    Nega Nessie
+"""), SpriteKind.enemy)
+myEnemySprite.say("LETS DO GREAT WORK!!!")
 mySprite.set_stay_in_screen(True)
 controller.move_sprite(mySprite, 100, 0)
 mySprite.ay = 500
 scene.camera_follow_sprite(mySprite)
 tiles.place_on_tile(mySprite, tiles.get_tile_location(0, 155))
+tiles.place_on_tile(myEnemySprite, tiles.get_tile_location(5, 157))
--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
     mySprite.vy = -300
 })
 let mySprite : Sprite = null
+let myEnemySprite : Sprite = null
 scene.setBackgroundColor(9)
 tiles.setCurrentTilemap(tilemap`
     level1
@@ -11,8 +12,13 @@ music.playMelody("A F A B C5 G A G ", 120)
 mySprite = sprites.create(assets.image`
     myImage
 `, SpriteKind.Player)
+myEnemySprite = sprites.create(assets.image`
+    Nega Nessie
+`, SpriteKind.Enemy)
+myEnemySprite.say("LETS DO GREAT WORK!!!")
 mySprite.setStayInScreen(true)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 500
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 155))
+tiles.placeOnTile(myEnemySprite, tiles.getTileLocation(5, 157))
