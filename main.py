6d606fd5b1834c0c7a0096e155e79412884996c0
Michael Thompson <actionscripted@gmail.com>
2022-07-15 15:27:11 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -1,43 +1,59 @@
-myEnemySprite: Sprite = None
-mySprite: Sprite = None
-game.set_game_over_effect(False, game.lose_effect)
-
-info.set_life(3)
-
-def change_text_interval():
-    myEnemySprite.sayText("What's our HIGHEST priority right now?")
-
-def change_text_interval_2():
-    myEnemySprite.sayText("LETS DO GREAT WORK!!!")
-
-def change_text_interval_3():
-    myEnemySprite.sayText("What's this project's STATUS?")
-
 def on_a_pressed():
-    mySprite.vy = -300
+    global gunType
+    if mySprite.is_hitting_tile(CollisionDirection.BOTTOM):
+        mySprite.vy = -300
+        gunType = "squirt"
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
-def my_overlap_function():
-    tiles.place_on_random_tile(myEnemySprite, assets.tile("binaryMid"))
+def on_left_pressed():
+    global isFacingLeft
+    isFacingLeft = 1
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_on_overlap(sprite, otherSprite):
+    global gunType
+    gunPickup.destroy()
+    gunType = "squirt"
+sprites.on_overlap(SpriteKind.food, SpriteKind.player, on_on_overlap)
+
+def on_right_pressed():
+    global isFacingLeft
+    isFacingLeft = 0
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def on_on_overlap2(sprite2, otherSprite2):
+    tiles.place_on_random_tile(myEnemySprite, assets.tile("""
+        binaryMid
+    """))
     info.change_life_by(-1)
     if info.life() <= 0:
         mySprite.destroy()
         game.over(False)
-    
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
 
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
-
-
-scene.set_background_color(9)
-tiles.set_current_tilemap(tilemap("""level1"""))
+isFacingLeft = 0
+gunPickup: Sprite = None
+myEnemySprite: Sprite = None
+mySprite: Sprite = None
+gunType = ""
+game.set_game_over_effect(False, game.lose_effect)
+info.set_life(3)
+scene.set_background_color(13)
+tiles.set_current_tilemap(tilemap("""
+    level1
+"""))
 music.set_volume(29)
 music.play_melody("A F A B C5 G A G ", 120)
+gunType = "none"
 mySprite = sprites.create(assets.image("""
     myImage
 """), SpriteKind.player)
 myEnemySprite = sprites.create(assets.image("""
     Nega Nessie
 """), SpriteKind.enemy)
-
+gunPickup = sprites.create(assets.image("""
+    Squirt Gun
+"""), SpriteKind.food)
 tiles.place_on_tile(myEnemySprite, tiles.get_tile_location(9, 157))
 myEnemySprite.follow(mySprite, 30)
 mySprite.set_stay_in_screen(True)
@@ -46,9 +62,35 @@ mySprite.ay = 500
 scene.camera_follow_sprite(mySprite)
 tiles.place_on_tile(mySprite, tiles.get_tile_location(3, 155))
 
+def on_on_update():
+    if isFacingLeft == 1:
+        if "squirt" == gunType:
+            mySprite.set_image(assets.image("""
+                nessieSquirt0
+            """))
+        else:
+            mySprite.set_image(assets.image("""
+                myImage5
+            """))
+    elif "squirt" == gunType:
+        mySprite.set_image(assets.image("""
+            nessieSquirt
+        """))
+    else:
+        mySprite.set_image(assets.image("""
+            myImage
+        """))
+    info.change_score_by(1)
+game.on_update(on_on_update)
 
-game.on_update_interval(5000, change_text_interval)
-game.on_update_interval(8000, change_text_interval_2)
-game.on_update_interval(12000, change_text_interval_3)
+def on_update_interval():
+    myEnemySprite.say_text("What's our HIGHEST priority right now?")
+game.on_update_interval(5000, on_update_interval)
 
-sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, my_overlap_function)
+def on_update_interval2():
+    myEnemySprite.say_text("What's this project's STATUS?")
+game.on_update_interval(12000, on_update_interval2)
+
+def on_update_interval3():
+    myEnemySprite.say_text("LETS DO GREAT WORK!!!")
+game.on_update_interval(8000, on_update_interval3)
--- main.ts
+++ main.ts
@@ -6,10 +6,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     isFacingLeft = 1
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    isFacingLeft = 0
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite2, otherSprite2) {
     tiles.placeOnRandomTile(myEnemySprite, assets.tile`binaryMid`)
     info.changeLifeBy(-1)
     if (info.life() <= 0) {
@@ -17,17 +14,28 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
         game.over(false)
     }
 })
+sprites.onOverlap(SpriteKind.Food, SpriteKind.Player, function (sprite, otherSprite) {
+    gunPickup.destroy()
+    gunType = "squirt"
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    isFacingLeft = 0
+})
 let isFacingLeft = 0
+let gunPickup: Sprite = null
 let myEnemySprite: Sprite = null
 let mySprite: Sprite = null
+let gunType = ""
 game.setGameOverEffect(false, game.loseEffect)
 info.setLife(3)
-scene.setBackgroundColor(9)
+scene.setBackgroundColor(13)
 tiles.setCurrentTilemap(tilemap`level1`)
 music.setVolume(29)
 music.playMelody("A F A B C5 G A G ", 120)
+gunType = "none"
 mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 myEnemySprite = sprites.create(assets.image`Nega Nessie`, SpriteKind.Enemy)
+gunPickup = sprites.create(assets.image`Squirt Gun`, SpriteKind.Food)
 tiles.placeOnTile(myEnemySprite, tiles.getTileLocation(9, 157))
 myEnemySprite.follow(mySprite, 30)
 mySprite.setStayInScreen(true)
@@ -35,9 +43,16 @@ controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 500
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 155))
+tiles.placeOnRandomTile(gunPickup, assets.tile`myTile0`)
 game.onUpdate(function () {
     if (isFacingLeft == 1) {
-        mySprite.setImage(assets.image`myImage4`)
+        if ("squirt" == gunType) {
+            mySprite.setImage(assets.image`nessieSquirt0`)
+        } else {
+            mySprite.setImage(assets.image`myImage5`)
+        }
+    } else if ("squirt" == gunType) {
+        mySprite.setImage(assets.image`nessieSquirt`)
     } else {
         mySprite.setImage(assets.image`myImage`)
     }
