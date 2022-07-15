3a59916fbfd5ee70e58cd9b0d5eb45d72057acbc
smezzacapo <32824995+smezzacapo@users.noreply.github.com>
2022-07-15 12:29:47 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
     mySprite.vy = -300
 })
-let mySprite: Sprite = null
+let mySprite : Sprite = null
+let myEnemySprite : Sprite = null
 scene.setBackgroundColor(9)
+tiles.setCurrentTilemap(tilemap`
+    level1
+`)
+mySprite = sprites.create(assets.image`
+    myImage
+`, SpriteKind.Player)
+myEnemySprite = sprites.create(assets.image`Nega Nessie`, SpriteKind.Enemy)
 tiles.setCurrentTilemap(tilemap`level1`)
+music.setVolume(29)
+music.playMelody("A F A B C5 G A G ", 120)
 mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 mySprite.setStayInScreen(true)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 500
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 1))
+tiles.placeOnTile(myEnemySprite, tiles.getTileLocation(5, 1))
+myEnemySprite.setStayInScreen(true)
