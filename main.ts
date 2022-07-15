640ef1aedbe44acf4c8b4c7e7dca27bbaaeaf0f2
Michael Thompson <actionscripted@gmail.com>
2022-07-15 14:54:50 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,33 @@
-let myEnemySprite : Sprite = null
-let mySprite : Sprite = null
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -300
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    isFacingLeft = 1
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    isFacingLeft = 0
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    tiles.placeOnRandomTile(myEnemySprite, assets.tile`binaryMid`)
+    info.changeLifeBy(-1)
+    if (info.life() <= 0) {
+        mySprite.destroy()
+        game.over(false)
+    }
+})
+let isFacingLeft = 0
+let myEnemySprite: Sprite = null
+let mySprite: Sprite = null
 game.setGameOverEffect(false, game.loseEffect)
 info.setLife(3)
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    mySprite.vy = -300
-})
 scene.setBackgroundColor(9)
 tiles.setCurrentTilemap(tilemap`level1`)
 music.setVolume(29)
 music.playMelody("A F A B C5 G A G ", 120)
-mySprite = sprites.create(assets.image`
-    myImage
-`, SpriteKind.Player)
-myEnemySprite = sprites.create(assets.image`
-    Nega Nessie
-`, SpriteKind.Enemy)
+mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+myEnemySprite = sprites.create(assets.image`Nega Nessie`, SpriteKind.Enemy)
 tiles.placeOnTile(myEnemySprite, tiles.getTileLocation(9, 157))
 myEnemySprite.follow(mySprite, 30)
 mySprite.setStayInScreen(true)
@@ -22,21 +35,19 @@ controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 500
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 155))
-game.onUpdateInterval(5000, function change_text_interval() {
+game.onUpdate(function () {
+    if (isFacingLeft == 1) {
+        mySprite.setImage(assets.image`myImage4`)
+    } else {
+        mySprite.setImage(assets.image`myImage`)
+    }
+})
+game.onUpdateInterval(5000, function () {
     myEnemySprite.sayText("What's our HIGHEST priority right now?")
 })
-game.onUpdateInterval(8000, function change_text_interval_2() {
-    myEnemySprite.sayText("LETS DO GREAT WORK!!!")
-})
-game.onUpdateInterval(12000, function change_text_interval_3() {
+game.onUpdateInterval(12000, function () {
     myEnemySprite.sayText("What's this project's STATUS?")
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function my_overlap_function() {
-    tiles.placeOnRandomTile(myEnemySprite, assets.tile`binaryMid`)
-    info.changeLifeBy(-1)
-    if (info.life() <= 0) {
-        mySprite.destroy()
-        game.over(false)
-    }
-    
+game.onUpdateInterval(8000, function () {
+    myEnemySprite.sayText("LETS DO GREAT WORK!!!")
 })
